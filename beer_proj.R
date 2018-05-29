aa3ecdcbbfde45133a015c8e18c944275577080a
Saul Tamariz <36432832+realsaul00@users.noreply.github.com>
2018-05-28 22:10:59 -0600

    Add files via upload

---
+++ beer_proj.R
@@ -0,0 +1,97 @@
+# import dataset from local file
+library(data.table)
+#df <- read.csv('recipe.csv', header=TRUE)
+df<-fread("recipe.csv", header=TRUE)
+# summary of dataset
+summary(df)
+# structure of dataframe
+str(df)
+# first couple of observations of dataframe
+head(df)
+# converting columns to appropriate data types
+df$BeerID <- as.factor(df$BeerID)
+df$Name <- as.factor(df$Name)
+df$URL <- as.factor(df$URL)
+df$Style <- as.factor(df$Style)
+df$StyleID <- as.factor(df$StyleID)
+df$UserId <- as.factor(df$UserId)
+df$PrimingMethod <- as.factor(df$PrimingMethod)
+df$PrimingAmount <- as.factor(df$PrimingAmount)
+df$BrewMethod <- as.factor(df$BrewMethod)
+df$SugarScale <- as.factor(df$SugarScale)
+df$PrimaryTemp <- as.numeric(df$PrimaryTemp)
+df$MashThickness <- as.numeric(df$MashThickness)
+df$BoilGravity <- as.numeric(df$BoilGravity)
+df$PitchRate <- as.factor(df$PitchRate)
+df$BoilTime <- as.numeric(df$BoilTime)
+#df$BoilTime <- as.factor(df$BoilTime)
+#df$PitchRate <- as.numeric(df$PitchRate)
+str(df)
+# summary of new dataframe
+summary(df)
+# summary of the most common beer styles 
+summary(df$Style)
+# creating subset of drinkable beer
+df1 = subset(df, ABV<14)
+df2 = subset(df1, ABV>3.2)
+df3 = subset(df2, FG>.9)
+df4 = subset(df3, FG<1.5)
+df5 = subset(df4, OG>.9)
+df6 = subset(df5, OG<1.5)
+df7 = subset(df6, IBU<120)
+df8 = subset(df7, IBU>8)
+# create a copy of subset
+dfs <- df8
+# summary of drinkable beer recipe dataframe
+summary(dfs)
+# import ggplot2
+library(ggplot2)
+# scatterplot using ggplot2 of "abv", "ibu", and "color
+abv_ibu_gg <- ggplot(dfs, aes(x=ABV, y=IBU, color=Color)) + geom_point() + labs(title="Scatterplot", x="ABV", y="IBU") 
+print(abv_ibu_gg)
+# understanding "PrimingMethod" feature 
+dfs$PrimingMethod[0:300]
+# understanding "PrimingAmount" feature
+dfs$PrimingAmount[0:300]
+# ABV density distribution curve
+abv_density <- qplot(ABV, data=dfs, geom="density", alpha=I(.5), 
+                     main="ABV Density Distribution", xlab="ABV", ylab="Density")
+print(abv_density)
+# IBU desity distribution curve
+ibu_density <- qplot(IBU, data=dfs, geom="density", alpha=I(.5), 
+                     main="IBU Density Distribution", xlab="IBU", ylab="Density")
+print(ibu_density)
+# Color density distribution curve
+color_density <- qplot(Color, data=dfs, geom="density", alpha=I(.5), 
+                       main="Color Density Distribution", xlab="Color", ylab="Density")
+print(color_density)
+# Efficiency density distribution curve
+efficiency_density <- qplot(Efficiency, data=dfs, geom="density", alpha=I(.5), 
+                            main="Efficiency Density Distribution", xlab="Efficiency", ylab="Density")
+print(efficiency_density)
+# BoilTime histogram
+boiltime_hist <- hist(dfs$BoilTime, col="red", main="BoilTime(Histogram)", xlab="BoilTime")
+# qplot of "FG" ,"OG", and "IBU"
+og_fg_qplot <- qplot(FG, OG, color=IBU, data=dfs)
+print(og_fg_qplot)
+# qplot of "IBU", "BoilTime", and "BrewMethod"
+boiltime_ibu_qplot <- qplot(IBU, BoilTime, color=BrewMethod, data=dfs)
+print(boiltime_ibu_qplot)
+# eliminating several features columns that are not needed
+beer <- dfs[,c(2:16, 18:20)]
+summary(beer)
+brew_allgrain <- subset(beer, BrewMethod=="All Grain")
+summary(brew_allgrain)
+brew_biab <- subset(beer, BrewMethod=="BIAB")
+summary(brew_biab)
+brew_extract <- subset(beer, BrewMethod=="extract")
+summary(brew_extract)
+brew_partial <- subset(beer, BrewMethod=="Partial Mash")
+summary(brew_partial)
+prim_temp_qplot <- qplot(Efficiency, PrimaryTemp , color=BrewMethod, data=beer)
+print(prim_temp_qplot)
+# ABV density distribution curve
+mash_density <- qplot(MashThickness, data=brew_allgrain, geom="density", alpha=I(.5), 
+                      main="MashThickness Density Distribution", xlab="MashThickness", ylab="Density")
+print(mash_density)
+
